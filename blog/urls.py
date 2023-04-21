d52e073647b0f11673872e431d1e92f7580f71ce
Ukomah <henryukomah@gmail.com>
2023-04-21 18:20:48 +0100

    creating views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Index
+from .views import Index, DetailArticleView
 
 urlpatterns = [
     path('', Index.as_view(), name='index' ),
+    path('<int:pk>/', DetailArticleView.as_view(), name='detail_article'),
+    
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Article
 # Create your views here.
 class Index(ListView):
     model = Article
     queryset = Article.objects.all().order_by('-date')
     template_name = 'blog/index.html'
-    paginate_by = 1
+    paginate_by = 2
 
 
-
+class DetailArticleView(DetailView):
+    model = Article
+    template_name = 'blog/blog_post.html'
 
